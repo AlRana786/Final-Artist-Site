d88717a3521a6e9b66bd71d325231e4a35427689
AlRana786 <120431160+AlRana786@users.noreply.github.com>
2023-02-27 18:30:38 -0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,64 @@
-// Look for .hamburger
+
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector("header .nav");
 
-// On click
+
 hamburger.addEventListener("click", function () {
-    // Toggle class "is-active"
+
     hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
+
     menu.classList.toggle("open");
 });
+
+
+const buttonOne = document.getElementById("button-1")
+const buttonTwo = document.getElementById("button-2")
+const buttonThree = document.getElementById("button-3")
+const buttonFour = document.getElementById("button-4") 
+
+
+
+buttonOne.addEventListener("click", showPopUp1)
+buttonTwo.addEventListener("click", showPopUp2)
+buttonThree.addEventListener("click", showPopUp3)
+buttonFour.addEventListener("click", showPopUp4)
+
+
+
+function showPopUp1(evt) {
+    evt.preventDefault();
+    closePopup ()
+    document.getElementById("popup-1").style.display="flex"
+    
+} 
+
+function showPopUp2(evt) {
+    evt.preventDefault();
+    closePopup ()
+    document.getElementById("popup-2").style.display="flex"
+} 
+
+function showPopUp3(evt) {
+    evt.preventDefault();
+    closePopup ()
+    document.getElementById("popup-3").style.display="flex"
+} 
+
+function showPopUp4(evt) {
+    evt.preventDefault();
+    closePopup ()
+    document.getElementById("popup-4").style.display="flex"
+} 
+
+function closePopup () {
+    document.getElementById("popup-1").style.display="none"
+    document.getElementById("popup-2").style.display="none"
+    document.getElementById("popup-3").style.display="none"
+    document.getElementById("popup-4").style.display="none"
+    
+ }
+
+ 
+ 
+    
+ 
